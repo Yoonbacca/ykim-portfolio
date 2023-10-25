52924eb0b7b7bde4d74670021ef8dabd20eb9dda
YoonJae Kim <yoonjaekim3@gmail.com>
2023-10-25 16:16:45 -0500

    Added desktop nav icons

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -17,7 +17,7 @@ const Sidenav = () => {
             {
                 nav ? (
                     <div className='fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20'>
-                        <a href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                        <a href="#home" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
                             <AiOutlineHome size={20} />
                             <span className='pl-4'>Home</span>
                         </a>
@@ -41,9 +41,28 @@ const Sidenav = () => {
                 ) 
                 : (
                     ''
-                )
-            }
+                )}
+                <div href="#home" className='md:block hidden fixed top-[25%] z-10'>
+                    <div className='flex flex-col'>
+                        <a href='#home' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+                            <AiOutlineHome size={20} />
+                        </a>
+                        <a href='#work' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+                            <GrProjects size={20} />
+                        </a>
+                        <a href='#projects' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+                            <AiOutlineProject size={20} />
+                        </a>
+                        <a href='#resume' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+                            <BsPerson size={20} />
+                        </a>
+                        <a href='#contact' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+                            <AiOutlineMail size={20} />
+                        </a>
+                    </div>
+                </div>
         </div>
+
     )
 }
 
