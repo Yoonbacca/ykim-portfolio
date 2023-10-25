6eb081c784cbc38ac04a5fbc5063adcb88eee1c9
YoonJae Kim <yoonjaekim3@gmail.com>
2023-10-25 16:05:23 -0500

    Finalized button icons for mobile nav

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,5 +1,7 @@
 import React, {useState} from 'react'
-import { AiOutlineMenu, AiOutlineHome } from 'react-icons/ai'
+import { AiOutlineMenu, AiOutlineHome, AiOutlineProject, AiOutlineMail } from 'react-icons/ai'
+import { BsPerson } from 'react-icons/bs'
+import { GrProjects } from 'react-icons/gr'
 
 const Sidenav = () => {
 
@@ -20,25 +22,25 @@ const Sidenav = () => {
                             <span className='pl-4'>Home</span>
                         </a>
                         <a href="#work" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
-                            <AiOutlineHome size={20} />
+                            <GrProjects size={20} />
                             <span className='pl-4'>Work</span>
                         </a>
                         <a href="#projects" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
-                            <AiOutlineHome size={20} />
+                            <AiOutlineProject size={20} />
                             <span className='pl-4'>Projects</span>
                         </a>
                         <a href="#resume" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
-                            <AiOutlineHome size={20} />
+                            <BsPerson size={20} />
                             <span className='pl-4'>Resume</span>
                         </a>
                         <a href="#contact" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
-                            <AiOutlineHome size={20} />
+                            <AiOutlineMail size={20} />
                             <span className='pl-4'>Contact</span>
                         </a>
                     </div>
                 ) 
                 : (
-                    <div></div>
+                    ''
                 )
             }
         </div>
