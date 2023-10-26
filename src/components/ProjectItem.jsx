9c5697968c82aca52407bcde99ac5b8dcc3df32e
YoonJae Kim <yoonjaekim3@gmail.com>
2023-10-25 19:53:25 -0500

    Added resume link, form, and separate site and deploy buttons

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -3,19 +3,21 @@ import React from 'react';
 
 const ProjectItem = ({img, title, link, repo}) => {
     return (
-        <a href={link}>
         <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#001b5e]'>
             <img src={img} alt="/" className='rounded-xl group-hover:opacity-10' />
             <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
                 <h3 className='text-2xl font-bold text-white tracking-wdier text-center'>
                     {title}
                 </h3>
-                <p className='pb-4 pt-2 text-white text-center'>Deployment</p>
-                <a href={repo}>
-                    <p className='text-center p-3 rounded-ld bg-white text-gray-8700 font-bold cursor-pointer text-lg'>Repository</p>
+                <p className='pb-4 pt-2 text-white text-center'></p>
+                <a href={link} target="_blank">
+                    <p className='text-center p-3 rounded-ld bg-gray-800 text-white font-bold cursor-pointer text-lg hover:opacity-10'>Deployment</p>
+                </a>
+                <a href={repo} target="_blank">
+                    <p className='text-center p-3 rounded-ld bg-white text-gray-8700 font-bold cursor-pointer text-lg hover:opacity-10'>Repo</p>
                 </a>
             </div>
-        </div></a>
+        </div>
     )
 }
 
