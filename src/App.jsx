31a3338d340e2619c4d4d1377b6fbaf836e82a76
YoonJae Kim <yoonjaekim3@gmail.com>
2023-10-25 19:22:19 -0500

    Added Project tiles

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Sidenav from './components/Sidenav';
 import Home from './components/Home';
 import Work from './components/Work';
+import Projects from './components/Projects';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,6 +12,7 @@ function App() {
       <Sidenav />
       <Home />
       <Work />
+      <Projects />
     </div>
   );
 }
